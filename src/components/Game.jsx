e64917cbccb2646c7b1e21259ce4ef8a979bf283
web-dev-scrim <webdevscrim2025@gmail.com>
2026-03-14 22:39:55 +0100

    Added end game functionality to tenzies

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,19 +1,36 @@
 import Dice from "./Dice";
-import React from "react";
+import { useState, useRef, useEffect } from "react";
+import { nanoid } from 'nanoid'
+import ReactConfetti from "react-confetti";
 
 export default function Game() {
-  const [dice, setDice] = React.useState([
-    { id: 1, value: 1, isHeld: false },
-    { id: 2, value: 2, isHeld: false },
-    { id: 3, value: 1, isHeld: false },
-    { id: 4, value: 4, isHeld: false },
-    { id: 5, value: 5, isHeld: false },
-    { id: 6, value: 3, isHeld: false },
-    { id: 7, value: 3, isHeld: false },
-    { id: 8, value: 5, isHeld: false },
-    { id: 9, value: 6, isHeld: false },
-    { id: 10, value: 1, isHeld: false }
-  ]);
+  
+  
+  function generateAllNewDice() {
+
+   return Array(10).fill().map(() => {
+      return {
+        id: nanoid(),
+        value: Math.floor(Math.random() * 6) + 1,
+        isHeld: false
+      }
+   }
+   )
+  }
+
+  const [dice, setDice] = useState(() => generateAllNewDice());//How lazy-initialization is used here to generate the initial state of the dice only once when the component mounts, rather than on every render. This can improve performance by avoiding unnecessary calculations on subsequent renders.
+
+  const isGameWon = dice.every(die => die.isHeld && die.value === dice[0].value);
+
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+
+    if (isGameWon) { 
+      buttonRef.current.focus();
+    }
+   }, [isGameWon]);
+
 
   function setDieHeld(id) {
     setDice(oldDice => {
@@ -31,15 +48,20 @@ export default function Game() {
     })
   }
 
+  function restartGame() {
+    setDice(generateAllNewDice());
+  }
+
   return (
     <section className="game">
+      {isGameWon && <ReactConfetti />}
       <h3 className="game-title">Tenzies</h3>
       <p className="game-description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <Dice
         dice={dice}
         setDieHeld={setDieHeld}
       ></Dice>
-      <button className="roll-btn" onClick={shuffleDice}>Roll</button>
+      <button ref={buttonRef} className="roll-btn" onClick={isGameWon ? restartGame : shuffleDice}>{isGameWon ? "New Game" : "Roll"}</button>
     </section>
   );
 }
